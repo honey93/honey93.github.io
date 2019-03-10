d930b2e24f0b188d42542f2ad7914b08492c9d89
Honey Thakuria <thakuria.honey@gmail.com>
2019-03-10 22:50:53 +0530

    Changes to the links

--- sw.js
+++ sw.js
@@ -6,20 +6,20 @@ workbox.precaching.precacheAndRoute([
     "revision": "e21bc095df8ef1542acb3b33983e1175"
   },
   {
+    "url": "/_nuxt/4eac702d7661baf9a28c.js",
+    "revision": "ff2c7b0b32ea994e0405587eb4a0b971"
+  },
+  {
     "url": "/_nuxt/88ec1ef62dcbbde0363d.js",
     "revision": "fa7212a754970a0cfc1b04e6926f56a6"
   },
   {
-    "url": "/_nuxt/c9c2850c55e607488950.js",
-    "revision": "9e160cc8f786355a3b21e1562b2dc64c"
+    "url": "/_nuxt/cee34cbc0e567914de8d.js",
+    "revision": "4a392ac53eee4262dda991e1acc31651"
   },
   {
     "url": "/_nuxt/e052f18e693337eb7c30.js",
     "revision": "481fee791280420ce7ec6b03c565fb18"
-  },
-  {
-    "url": "/_nuxt/eb4986b4e00945256ba9.js",
-    "revision": "b250e63f14521323e624e2c030683ad8"
   }
 ], {
   "cacheId": "honeythakuria",
